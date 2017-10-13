4c5170886ef59e0c583864fbd23708af9f25dbdd
Kim JiHyun <kcjohnnykim@gmail.com>
2017-10-13 14:29:57 +0900

    20171013

--- Codeforce_Educational/Round30_ProbA.cpp
+++ Codeforce_Educational/Round30_ProbA.cpp
@@ -1,41 +1,41 @@
-////20171013 10:55 ~ 11:03
-//#ifdef _MSC_VER
-//#define _CRT_SECURE_NO_WARNINGS
-//#endif
-//
-//#define sd(x) scanf(" %d", &x);
-//#define slld(x) scanf(" %lld", &x);
-//#define ll long long
-//#define pb push_back
-//#define xx first
-//#define yy second
-//
-//#include <iostream>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string>
-//#include <memory.h>
-//#include <algorithm>
-//#include <vector>
-//#include <queue>
-//#include <stack>
-//#include <math.h>
-//using namespace std;
-//
-//int n, k, x;
-//int ans = 0;
-//
-//int main() {
-//	cin >> n >> k >> x;
-//	for (int i = 1, temp; i <= n - k; i++) {
-//		cin >> temp;
-//		ans += temp;
-//	}
-//	for (int i = 1, temp; i <= k; i++) {
-//		cin >> temp;
-//		ans += x;
-//	}
-//	cout << ans << endl;
-//	system("pause");
-//	return 0;
-//}
+//20171013 10:55 ~ 11:03
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define ll long long
+#define pb push_back
+#define xx first
+#define yy second
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+using namespace std;
+
+int n, k, x;
+int ans = 0;
+
+int main() {
+	cin >> n >> k >> x;
+	for (int i = 1, temp; i <= n - k; i++) {
+		cin >> temp;
+		ans += temp;
+	}
+	for (int i = 1, temp; i <= k; i++) {
+		cin >> temp;
+		ans += x;
+	}
+	cout << ans << endl;
+	system("pause");
+	return 0;
+}
