03cb2b55fc3a96648fa8c876f4648d215733d4f0
Kim JiHyun <kcjohnnykim@gmail.com>
2018-02-16 01:05:00 +0900

    20180216

--- acmicpc.net February 2018/20180213/1328.cpp
+++ acmicpc.net February 2018/20180213/1328.cpp
@@ -1,19 +1,28 @@
 //20180213 10:24 ~ 
+//20180215 23:29 ~ 01:02
 #include <iostream>
 using namespace std;
 
+typedef long long ll;
 const int MOD = 1000000007;
-int n, l, r;
+ll n, l, r;
 
-int ldp[101][101];
-int rdp[101][101];
+ll dp[101][101][101]; //dp[n][l][r];
 
 int main(){
     cin>>n>>l>>r;
-    ldp[0][0] = rdp[0][0] = ldp[1][1] = rdp[1][1] = 1;
-    for(int i=2; i<=n; i++){
-        for(int j=0; j<i; j++){
-            
+    dp[1][1][1] = 1;
+    dp[2][1][2] = dp[2][2][1] = 1;
+    for(int i=3; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            for(int k=1; k<=i; k++){
+                //dp[i][j][k] += (dp[i-1][j-1][k] + dp[i-1][j][k-1] + dp[i-1][j][k]);
+                dp[i][j][k] = (dp[i][j][k] + dp[i-1][j-1][k])%MOD;
+                dp[i][j][k] = (dp[i][j][k] + dp[i-1][j][k-1])%MOD;
+                dp[i][j][k] = (dp[i][j][k] + dp[i-1][j][k]*(i-2))%MOD;
+            }
         }
     }
+    cout<<dp[n][l][r]<<endl;
+    return 0;
 }
