7f9081ec0534b737518d85573f4cb40978ef3dd3
Kim JiHyun <kcjohnnykim@gmail.com>
2018-10-24 00:17:37 +0900

    20181024

--- Codeforce October 2018/1065B.cpp
+++ Codeforce October 2018/1065B.cpp
@@ -2,15 +2,25 @@
 #include <iostream>
 using namespace std;
 
-int n, m;
-int minAns, maxAns;
+long long n, m;
+long long minAns, maxAns;
 
 int main() {
 	cin >> n >> m;
-	maxAns = n - (m + 1);
 	if (m >= (n / 2 + 1)) minAns = 0;
 	else minAns = n - (m * 2);
+	if (m == 0) maxAns = n;
+	else {
+		long long curr = 2;
+		maxAns = n - 2;
+		m--;
+		while (m>0) {
+			maxAns--;
+			m -= curr;
+			curr++;
+		}
+	}
 	cout << minAns << " " << maxAns << endl;
-	cin >> n;
+
 	return 0;
 }
