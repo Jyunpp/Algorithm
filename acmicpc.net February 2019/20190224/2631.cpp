3a81194c13b55b95716710b9b7b86759dc13b096
Kim JiHyun <kcjohnnykim@gmail.com>
2019-02-24 18:46:17 +0900

    20190224 LIS O(N^2)

---
+++ acmicpc.net February 2019/20190224/2631.cpp
@@ -0,0 +1,48 @@
+// 20190224 18:36 ~ 18:42
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+using namespace std;
+
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+
+int dp[201];
+vector<int> v;
+int n, a, ret = 201;
+
+int main() {
+    cin>>n;
+    rep(i, 0, n) {
+        cin>>a; v.pb(a);
+    }
+    rep(i, 0, n) {
+        dp[i] = 1;
+        rep(j, 0, i) {
+            if(v[j] < v[i]){
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    rep(i, 0, n) {
+        ret = min(ret, n - dp[i]);
+    }
+    cout<<ret<<endl;
+}
