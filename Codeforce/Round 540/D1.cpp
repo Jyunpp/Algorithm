391a06ae439bb7329a1a875cd26c44a02d5b09f4
Kim JiHyun <kcjohnnykim@gmail.com>
2019-03-04 22:12:04 +0900

    20190304 math problem

---
+++ Codeforce/Round 540/D1.cpp
@@ -0,0 +1,55 @@
+// 20190304 21:40 ~ 22:10
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+using namespace std;
+
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+
+int n, m, ret;
+
+int main() {
+    cin>>n>>m;
+    vector<int> v;
+    int temp = 0;
+    rep(i, 1, n+1) {
+        int a; cin>>a;
+        temp += a;
+        v.push_back(a);
+    }
+    if(temp < m) {
+        cout<<-1<<endl;
+        return 0;
+    }
+    sort(v.begin(), v.end(), [](int a, int b){return a>b;});
+    rep(i, 1, n+1) {
+        int sum = 0;
+        rep(j, 0, n) {
+            sum += max(v[j] - j/i, 0);
+        }
+        if(sum >=m) {
+            ret = i;
+            break;
+        }
+    }
+    cout<<ret<<endl;
+    return 0;
+}
