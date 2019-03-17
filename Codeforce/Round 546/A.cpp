6b8cd965cc67fc75b6cd7de512cad7440817b95d
Kim JiHyun <kcjohnnykim@gmail.com>
2019-03-17 17:02:16 +0900

    Codeforces Round 546 Div2, solved A

---
+++ Codeforce/Round 546/A.cpp
@@ -0,0 +1,45 @@
+// 20190317 16:56 ~ 
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+using namespace std;
+
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+
+int n, k;
+P arr[101];
+
+int main() {
+    cin>>n;
+    rep(i, 1, n+1) {
+        int a, b; cin>>a>>b;
+        arr[i] = {a, b};
+    }
+    cin>>k;
+    rep(i, 1, n+1) {
+        if(k<=arr[i].yy) {
+            cout<<n+1-i<<endl;
+            return 0;
+        }
+    }
+    cout<<0<<endl;
+    return 0;
+}
