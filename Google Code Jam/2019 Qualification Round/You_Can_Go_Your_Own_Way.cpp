a4187c4696535f2e6af61bd8b7317650b0f012e2
Kim JiHyun <kcjohnnykim@gmail.com>
2019-04-06 10:37:24 +0900

    Google Code Jam 2019 Q Round solve 1,2,3 - i'll update after the end of the round

---
+++ Google Code Jam/2019 Qualification Round/You_Can_Go_Your_Own_Way.cpp
@@ -0,0 +1,34 @@
+// round : 20190406 08:00 ~ 20190407 11:00
+// 20190406 09:26 ~ 09:32
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <map>
+using namespace std;
+
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+
+int T;
+
+int main() {
+    // i will update after the end of the round
+    return 0;
+}
