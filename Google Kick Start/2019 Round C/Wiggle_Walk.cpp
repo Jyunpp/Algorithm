bd7a6bdf7fc167b0ce17149ad52ae01ee6e62fa1
Kim JiHyun <kcjohnnykim@gmail.com>
2019-05-29 03:18:56 +0900

    Google Kick Start 2019 round C wiggle walk fix MLE (int->bool)

--- Google Kick Start/2019 Round C/Wiggle_Walk.cpp
+++ Google Kick Start/2019 Round C/Wiggle_Walk.cpp
@@ -11,7 +11,7 @@
 #include <math.h>
 #include <map>
 using namespace std;
-
+ 
 #define sd(x) scanf(" %d", &x);
 #define slld(x) scanf(" %lld", &x);
 #define pb push_back
@@ -20,17 +20,17 @@ using namespace std;
 #define rep(i, l, r) for(int i=l; i<r; i++)
 #define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
 #define powll(a, b) (ll) pow(a, b)
-
+ 
 typedef long long ll;
 typedef pair<int, int> P;
 typedef pair<ll, ll> Pll;
-
+ 
 int t;
 P ret;
 int n, r, c, sr, sc;
-vector<vector<int>> visited;
+vector<vector<bool>> visited;
 int dy[128], dx[128];
-
+ 
 int main() {
     dy['E'] = 0; dx['E'] = 1;
     dy['W'] = 0; dx['W'] = -1;
@@ -40,19 +40,19 @@ int main() {
     cin>>t;
     rep(T, 1, t+1) {
         cin>>n>>r>>c>>sr>>sc;
-        visited = vector<vector<int>>(r+1, vector<int>(c+1));
-        visited[sr][sc]++;
+        visited = vector<vector<bool>>(r+1, vector<bool>(c+1));
+        visited[sr][sc] = true;
         
         rep(i, 1, n+1) {
             char ch; cin>>ch;
             sr += dy[ch];
             sc += dx[ch];
-            while(visited[sr][sc] != 0) {
+            while(visited[sr][sc] != false) {
                 sr += dy[ch];
                 sc += dx[ch];
             }
             // cout<<"debug sr: "<<sr<<", sc:"<<sc<<endl;
-            visited[sr][sc]++;
+            visited[sr][sc] = true;
             if(i==n) ret = {sr, sc};
         }
         cout<<"Case #"<<T<<": "<<ret.xx<<" "<<ret.yy<<endl;
